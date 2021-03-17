80d3d6a4273322dc4f526ac0199d19e95081ceb4
Uzayisenga <aminauzayisenga@gmail.com>
2021-03-17 08:54:34 +0200

    make rspec

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do |t|
+    user = User.create! :first_name => "Hash#{t}",
+                        :last_name => "Mahad1 #{t}",
+                        :city => "Unknown#{t}",
+                        :username => 'Mahad',
+                        :address => "Somalia#{t}",
+                        :primary_practice  => "Nurse#{t}",
+                        :secondary_practice=> "Pediatry#{t}",
+                        :gcmid => 'Blood pressure'
+  
+  end
+
+  
